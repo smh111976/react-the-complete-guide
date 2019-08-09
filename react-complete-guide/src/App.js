73edea0bc9b0e36f30f2197d2f5474e61531f4c9
Scott Henning <scott.henning@altramotion.com>
2019-08-09 09:05:44 -0500

    added useState hook to App.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,26 +11,30 @@ const app = props => {
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some over value'
+    ]
   });
+
+  const [otherState, setOtherState] = useState('some over value');
+
+  console.log(personsState, otherState);
 
   const switchNameHandler = () => {
     // console.log('Was clicked!');
 
-    this.setState({
+    setPersonsState({
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
-      ]
+      ],
+      otherState: personsState.otherState
     });
   };
   return (
     <div className='App'>
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <button onClick={switchNameHandler}>Switch Name</button>
       <Person
         name={personsState.persons[0].name}
         age={personsState.persons[0].age}

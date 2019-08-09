888ec4cd8aa4cda76cad09796c8c64fd1aa8c3d2
Scott Henning <scott.henning@altramotion.com>
2019-08-09 07:26:01 -0500

    updates to App, addition of Person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const app = props => {
+  // useState returns an array with exactly 2 elements
+  // the first element is the current state,
+  // the second element is function that allows us to update state
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some over value'
+  });
 
-export default App;
+  const switchNameHandler = () => {
+    // console.log('Was clicked!');
+
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+  return (
+    <div className='App'>
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      >
+        {personsState.otherState}
+      </Person>
+    </div>
+  );
+};
+
+export default app;
